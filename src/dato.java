ec65327720bc84f45b8f5e2ea0121d4fed33b069
alla2297 <alla2297@edu.easj.dk>
2021-09-07 18:11:24 +0200

    Initial commit

---
+++ src/dato.java
@@ -0,0 +1,19 @@
+public class dato {
+    public static void main(String[] args) {
+        // pick a random number
+        String day = "tirsdag";
+        int date= 07 ;
+        String mount = "september";
+        int year = 2021;
+        System.out.print("god morgen i dag er det: ");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.print(mount);
+        System.out.print(", ");
+        System.out.print(year);
+        System.out.println(".");
+
+    }
+}
